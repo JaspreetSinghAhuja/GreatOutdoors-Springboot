6029db415589e83d3e8fa588eb14a6a80ae3c90f
gautam1202 <gautam1202@users.noreply.github.com>
2020-07-26 22:55:59 +0530

    Wishlist Table

---
+++ GreatOutdoorsProductManagementSystem/src/main/java/com/capg/GO_Product_Management/entity/WishlistProduct.java
@@ -0,0 +1,68 @@
+package com.capg.GO_Product_Management.entity;
+
+import javax.persistence.Column;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "Wishlist_Table")
+public class WishlistProduct {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int productUserId;
+
+	@Column(length = 10)
+	private long productId;
+
+	@ManyToOne
+	@JoinColumn(name = "userId")
+	private User user;
+
+	public WishlistProduct(long productId) {
+		super();
+
+		this.productId = productId;
+
+	}
+
+	public WishlistProduct() {
+		super();
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public int getProductUserId() {
+		return productUserId;
+	}
+
+	public void setProductUserId(int productUserId) {
+		this.productUserId = productUserId;
+	}
+
+	public long getProductId() {
+		return productId;
+	}
+
+	public void setProductId(long productId) {
+		this.productId = productId;
+	}
+
+	@Override
+	public String toString() {
+		return "WishlistProduct [productUserId=" + productUserId + ", productId=" + productId + ", user=" + user + "]";
+	}
+
+}
